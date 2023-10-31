f5846801c151ce5733c8c4f38cae7b13bd64936d
Sebastian Baldini <sebastian.baldini@outlook.com>
2023-10-31 16:33:27 -0400

    using microsecond delay instead of analogWrite

--- Control_Box_PWM_Code/Control_Box_PWM_Code.ino
+++ Control_Box_PWM_Code/Control_Box_PWM_Code.ino
@@ -2,6 +2,7 @@ int PWMPin = 3;
 int SoftStopPin = 14;
 int ThrottlePin = 15;
 int ThrottlePercent = 0;
+float DutyOn = 0;
 void setup() {
   pinMode(PWMPin, OUTPUT);
   pinMode(SoftStopPin, INPUT);
@@ -11,8 +12,13 @@ void setup() {
 void loop() {
   bool stop_check = digitalRead(SoftStopPin);
   if(stop_check){
-    ThrottlePercent = (analogRead(ThrottlePin)/1024)*256-1;
-    analogWrite(PWMPin, ThrottlePercent);
+    ThrottlePercent = (analogRead(ThrottlePin)/1023);
+    DutyOn = (0.05+ (ThrottlePercent * 0.05));
+    digitalWrite(PWMPin, HIGH);
+    delayMicroseconds(int((DutyOn)*18181));
+    digitalWrite(PWMPin, LOW);
+    delayMicroseconds(int((1-DutyOn)*18181));
+
   }
   else{
     ThrottlePercent = 0;
