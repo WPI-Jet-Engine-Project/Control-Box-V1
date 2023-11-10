9330a88e6610af789b5d899eb62c8cfe54530a24
Sebastian Baldini <sebastian.baldini@outlook.com>
2023-11-09 19:51:36 -0500

    hi kevin :)

--- Control_Box_PWM_Code/Control_Box_PWM_Code.ino
+++ Control_Box_PWM_Code/Control_Box_PWM_Code.ino
@@ -1,27 +1,21 @@
 int PWMPin = 3;
-int SoftStopPin = 14;
+// int SoftStopPin = 14;
 int ThrottlePin = 15;
-int ThrottlePercent = 0;
+int Throttle;
+float ThrottlePercent;
 float DutyOn = 0;
+int DutyTime = 0;
 void setup() {
-  pinMode(PWMPin, OUTPUT);
-  pinMode(SoftStopPin, INPUT);
-  pinMode(ThrottlePin, INPUT);
+  pinMode(11, OUTPUT);
+  TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM20);
+  TCCR2B = TCCR2B & 0b11111000 | 0x07;
+  OCR2A = 180;
+  OCR2B = 50;
+  Serial.begin(9600);
 }
-
+// fucking breaks below 100ish Hz kill me
 void loop() {
-  bool stop_check = digitalRead(SoftStopPin);
-  if(stop_check){
-    ThrottlePercent = (analogRead(ThrottlePin)/1023);
-    DutyOn = (0.05+ (ThrottlePercent * 0.05));
-    digitalWrite(PWMPin, HIGH);
-    delayMicroseconds(int((DutyOn)*18181));
-    digitalWrite(PWMPin, LOW);
-    delayMicroseconds(int((1-DutyOn)*18181));
-
-  }
-  else{
-    ThrottlePercent = 0;
-    }
-    
+  int throttleRead = analogRead(A1);
+  int throttle = 256*throttleRead/1024;
+  analogWrite(11,throttle);
 }
