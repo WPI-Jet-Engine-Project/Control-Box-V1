4a54910213b94dd2348d6ede9257b77fc42d451a
Kevin Siegall <digimonlord2@gmail.com>
2023-11-10 22:10:40 -0500

    Overhauled timer setup - setDutyCycle method, all untested for now

--- Control_Box_PWM_Code/Control_Box_PWM_Code.ino
+++ Control_Box_PWM_Code/Control_Box_PWM_Code.ino
@@ -1,21 +1,58 @@
-int PWMPin = 3;
-// int SoftStopPin = 14;
-int ThrottlePin = 15;
-int Throttle;
-float ThrottlePercent;
-float DutyOn = 0;
-int DutyTime = 0;
+// 8-bit PWM out
+// 10-bit ADC in
+
+// TODO: Minimize use of float operations without losing precision by doing some funky math shenanigans
+
+// Function Prototypes
+void setDutyCycle(float percentage);
+
+// pwmPin can be either 3 or 11 in order to use timer 2 for proper pwm configuration
+int pwmPin = 3;
+
+int armingSwitchPin = 14;
+int throttlePin = A1;
+
+int trimValue = 64;
+
 void setup() {
-  pinMode(11, OUTPUT);
-  TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM20);
-  TCCR2B = TCCR2B & 0b11111000 | 0x07;
-  OCR2A = 180;
-  OCR2B = 50;
+  pinMode (pwmPin, OUTPUT);
+
+  // Reset prescalers
+  GTCCR = bit (PSRASY);        // reset prescaler now
+
+  // mode 15
+  TCCR2A = bit (WGM20) | bit (WGM21) | bit (COM2B1); // fast PWM, clear OC2B on compare
+  
+  // start Timer 2
+  TCCR2B = bit (WGM22) | bit (CS20) | bit (CS22); // prescaler of 128
+  // 16,000,000 Hz / 128 (prescaler) = 125,000 / 50Hz = 2500 timer ticks per PWM pulse
+  OCR2A = 2500 - 1;
+  // The clock counts up to 2500 and then jumps down to zero (sawtooth wave)
+  // When that counter is above the set value for OCR2B, the pulse width is set high.
+  // Therefore, to set the duty cycle in %, you need to do a little math (see function below)
+  OCR2B = 2500; // 0% duty cycle to start
+  }  // end of setup
   Serial.begin(9600);
 }
-// fucking breaks below 100ish Hz kill me
+
+/* Max throttle increases duty cycle by 2.25%
+ * Trim increases duty cycle by ~0.005%/tick
+ * Base duty cycle is 5.75%
+ */
+
 void loop() {
-  int throttleRead = analogRead(A1);
-  int throttle = 256*throttleRead/1024;
-  analogWrite(11,throttle);
+  if(digitalRead(armingSwitchPin)){
+    int throttleRead = analogRead(throttlePin);
+    // TODO: With default trim of 64 (trimValue variable), calculate what the % duty cycle is and set it
+    setDutyCycle(0.0575);
+  } else{
+    setDutyCycle(0);
+  }
+}
+
+void setDutyCycle(float percentage){
+  // The clock counts up to 2500 (for 50Hz) and then jumps down to zero (sawtooth wave)
+  // When that counter is above the set value for OCR2B, the pulse width is set high.
+  // Therefore, to set the duty cycle in %, you need to multiply the max (2500) by (1-%)
+  OCR2B = (int) (2500*(1-percentage));
 }
